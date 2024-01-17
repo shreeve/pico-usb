5a9c90f8e4192e0e679d7dec684a8fbadec24908
Steve Shreeve <steve.shreeve@gmail.com>
2024-01-16 21:32:54 -0700

    Add some host specific code to hoho

--- hoho/hoho.c
+++ hoho/hoho.c
@@ -494,39 +494,59 @@ struct usb_device {
 //         panic("Unhandled IRQ 0x%x\n", (uint) (status ^ handled));
 //     }
 // }
-//
-// // ==[ Hardware reset ]========================================================
-//
-// // reset USB system
-// void usb_device_init() {
-//     reset_block(RESETS_RESET_USBCTRL_BITS);
-//     unreset_block_wait(RESETS_RESET_USBCTRL_BITS);
-//     memset(usb_dpram, 0, sizeof(*usb_dpram));
-//     irq_set_enabled(USBCTRL_IRQ, true);
-//     usb_hw->muxing = USB_USB_MUXING_TO_PHY_BITS | USB_USB_MUXING_SOFTCON_BITS;
-//     usb_hw->pwr = USB_USB_PWR_VBUS_DETECT_BITS | USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS;
-//     usb_hw->main_ctrl = USB_MAIN_CTRL_CONTROLLER_EN_BITS;
-//     usb_hw->sie_ctrl = USB_SIE_CTRL_EP0_INT_1BUF_BITS;
-//     usb_hw->inte = USB_INTS_BUFF_STATUS_BITS |
-//                    USB_INTS_BUS_RESET_BITS |
-//                    USB_INTS_SETUP_REQ_BITS;
-//     usb_setup_endpoints();
-//     usb_hw_set->sie_ctrl = USB_SIE_CTRL_PULLUP_EN_BITS;
-// }
-//
-// // ==[ Main ]==================================================================
-//
-// int main(void) {
-//     stdio_init_all();
-//     printf("USB Device Low-Level hardware example\n");
-//     usb_device_init();
-//
-//     // Wait until configured
-//     while (!configured) { tight_loop_contents(); }
-//
-//     // Get ready to rx from host
-//     usb_start_transfer(usb_get_endpoint_configuration(EP1_OUT_ADDR), NULL, 64);
-//
-//     // Everything is interrupt driven so just loop here
-//     while (1) { tight_loop_contents(); }
-// }
+
+// ==[ Hardware reset ]========================================================
+
+// reset USB host
+bool usb_host_init() {
+
+    // Reset controller
+    reset_block       (RESETS_RESET_USBCTRL_BITS);
+    unreset_block_wait(RESETS_RESET_USBCTRL_BITS);
+
+    // Clear state
+    memset(usb_hw   , 0, sizeof(*usb_hw   ));
+    memset(usb_dpram, 0, sizeof(*usb_dpram));
+    irq_set_enabled(USBCTRL_IRQ, true);
+
+    // Setup host mode
+    usb_hw->muxing    = USB_USB_MUXING_TO_PHY_BITS              |
+                        USB_USB_MUXING_SOFTCON_BITS             ;
+    usb_hw->pwr       = USB_USB_PWR_VBUS_DETECT_BITS            |
+                        USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS;
+    usb_hw->main_ctrl = USB_MAIN_CTRL_CONTROLLER_EN_BITS        |
+                        USB_MAIN_CTRL_HOST_NDEVICE_BITS         ;
+    usb_hw->sie_ctrl  = USB_SIE_CTRL_SOF_EN_BITS                |
+                        USB_SIE_CTRL_KEEP_ALIVE_EN_BITS         |
+                        USB_SIE_CTRL_PULLDOWN_EN_BITS           |
+                        USB_SIE_CTRL_EP0_INT_1BUF_BITS          ;
+    usb_hw->inte      = USB_INTE_BUFF_STATUS_BITS               |
+                        USB_INTE_HOST_CONN_DIS_BITS             |
+                        USB_INTE_HOST_RESUME_BITS               |
+                        USB_INTE_STALL_BITS                     |
+                        USB_INTE_TRANS_COMPLETE_BITS            |
+                        USB_INTE_ERROR_RX_TIMEOUT_BITS          |
+                        USB_INTE_ERROR_DATA_SEQ_BITS            ;
+
+    usb_setup_endpoints();
+    usb_hw_set->sie_ctrl = USB_SIE_CTRL_PULLUP_EN_BITS;
+
+    return true;
+}
+
+// ==[ Main ]==================================================================
+
+int main(void) {
+    stdio_init_all();
+    printf("USB Host example\n");
+    usb_host_init();
+
+    // Wait until configured
+    while (!configured) { tight_loop_contents(); }
+
+//    // Get ready to rx from host
+//    usb_start_transfer(usb_get_endpoint_configuration(EP1_OUT_ADDR), NULL, 64);
+
+    // Everything is interrupt driven so just loop here
+    while (1) { tight_loop_contents(); }
+}
