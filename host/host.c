7055e356f7695623eaf303cea0c8f1cc13a16da6
Steve Shreeve <steve.shreeve@gmail.com>
2024-01-25 16:06:40 -0700

    More host init/reset work

--- host/host.c
+++ host/host.c
@@ -71,31 +71,30 @@ void usb_host_reset() {
     // Clear state
     memset(usb_hw   , 0, sizeof(*usb_hw   ));
     memset(usb_dpram, 0, sizeof(*usb_dpram));
-    irq_set_enabled(USBCTRL_IRQ, true);
 
     // Setup host mode
-    usb_hw->muxing    = USB_USB_MUXING_TO_PHY_BITS              |
-                        USB_USB_MUXING_SOFTCON_BITS             ;
-    usb_hw->pwr       = USB_USB_PWR_VBUS_DETECT_BITS            |
-                        USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS;
-    usb_hw->main_ctrl = USB_MAIN_CTRL_CONTROLLER_EN_BITS        |
-                        USB_MAIN_CTRL_HOST_NDEVICE_BITS         ;
-    usb_hw->sie_ctrl  = USB_SIE_CTRL_EP0_INT_1BUF_BITS          |
-                        USB_SIE_CTRL_SOF_EN_BITS                |
-                        USB_SIE_CTRL_KEEP_ALIVE_EN_BITS         |
-                        USB_SIE_CTRL_PULLDOWN_EN_BITS           ;
-    usb_hw->inte      = USB_INTE_BUFF_STATUS_BITS               |
-                        USB_INTE_HOST_CONN_DIS_BITS             |
-                        USB_INTE_HOST_RESUME_BITS               |
-                        USB_INTE_STALL_BITS                     |
-                        USB_INTE_TRANS_COMPLETE_BITS            |
-                        USB_INTE_ERROR_RX_TIMEOUT_BITS          |
-                        USB_INTE_ERROR_DATA_SEQ_BITS            ;
+    usb_hw->muxing    = USB_USB_MUXING_TO_PHY_BITS              | // Connect USB Phy
+                        USB_USB_MUXING_SOFTCON_BITS             ; // TODO: What is this?
+    usb_hw->pwr       = USB_USB_PWR_VBUS_DETECT_BITS            | // Enable VBUS detection
+                        USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS; // Enable VBUS detection
+    usb_hw->main_ctrl = USB_MAIN_CTRL_CONTROLLER_EN_BITS        | // Enable controller
+                        USB_MAIN_CTRL_HOST_NDEVICE_BITS         ; // Host mode
+    usb_hw->sie_ctrl  = USB_SIE_CTRL_VBUS_EN_BITS               | // Supply VBUS to device
+                        USB_SIE_CTRL_SOF_EN_BITS                | // Enable full speed bus
+                        USB_SIE_CTRL_KEEP_ALIVE_EN_BITS         | // Enable low speed bus
+                        USB_SIE_CTRL_PULLDOWN_EN_BITS           ; // Enable devices to connect
+    usb_hw->inte      = USB_INTE_HOST_CONN_DIS_BITS             | // Device connect/disconnect
+                        USB_INTE_STALL_BITS                     | // Stall detected
+                        USB_INTE_BUFF_STATUS_BITS               | // Buffer ready
+                        USB_INTE_TRANS_COMPLETE_BITS            | // Transfer complete
+                        USB_INTE_HOST_RESUME_BITS               | // Device resumed
+                        USB_INTE_ERROR_DATA_SEQ_BITS            | // Data error
+                        USB_INTE_ERROR_RX_TIMEOUT_BITS          ; // Receive timeout
 
     // usb_setup_endpoints();
-    usb_hw_set->sie_ctrl = USB_SIE_CTRL_PULLUP_EN_BITS;
+    irq_set_enabled(USBCTRL_IRQ, true); // irq_set_exclusive_handler(USBCTRL_IRQ, isr_usbctrl);
+    printf("USB host reset\n\n");
 
-    return true;
 }
 
 // ==[ Main ]==================================================================
