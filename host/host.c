2802d961a32c0171e0b62c63d270197ba4ff8b32
Steve Shreeve <steve.shreeve@gmail.com>
2024-01-25 16:00:49 -0700

    Fleshing out with hexdump and some headers

--- host/host.c
+++ host/host.c
@@ -2,7 +2,62 @@
 
 #include "usb_common.h"
 
+// usb_endpoint_descriptor
+// usb_interface_descriptor
+// usb_configuration_descriptor
+// usb_device_descriptor
+
 // ==[ PicoUSB ]===============================================================
+
+#include <stdio.h> // For printf
+#include <string.h> // For memcpy
+
+#include "pico/stdlib.h" // Pico stdlib
+#include "hardware/regs/usb.h" // USB hardware registers from pico-sdk
+#include "hardware/structs/usb.h" // USB hardware structs from pico-sdk
+#include "hardware/irq.h" // For interrupt enable and numbers
+#include "hardware/resets.h" // For resetting the native USB controller
+#include "hardware/sync.h" // For __dmb()
+
+#define usb_hw_set   ((usb_hw_t *) hw_set_alias_untyped  (usb_hw))
+#define usb_hw_clear ((usb_hw_t *) hw_clear_alias_untyped(usb_hw))
+
+// ==[ Transfers ]=============================================================
+
+// Hex dump (mode: 0 = hex; 1 = hex + ascii; 2 = hex + ascii + no newline)
+void hexdump(const void* data, size_t size, uint mode) {
+    const unsigned char* byte = (const unsigned char *) data;
+    size_t i, j;
+
+    for (i = 0; i < size; i += 16) {
+        printf("\t| %08zx | ", i); // Print the offset
+
+        // Print hex values
+        for (j = 0; j < 16; j++) {
+            if (i + j < size) {
+                printf("%02x ", byte[i + j]);
+            } else {
+                printf("   "); // Pad if less than 16 bytes in the line
+            }
+        }
+
+        printf(" | ");
+
+        if (mode > 1) return;
+
+        // Print ASCII values
+        if (mode == 1) {
+            for (j = 0; j < 16; j++) {
+                if (i + j < size) {
+                    unsigned char ch = byte[i + j];
+                    printf("%c", (ch >= 32 && ch <= 126) ? ch : '.');
+                }
+            }
+        }
+
+        printf("\n");
+    }
+}
 
 // ==[ Resets ]================================================================
 
