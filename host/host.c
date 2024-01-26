5914637773bfff50f98200b459f28b9b422e72c9
Steve Shreeve <steve.shreeve@gmail.com>
2024-01-26 03:05:34 -0700

    Shuffling

--- host/host.c
+++ host/host.c
@@ -170,8 +170,8 @@ void usb_host_reset() {
     // Setup host mode
     usb_hw->muxing    = USB_USB_MUXING_TO_PHY_BITS               | // Connect USB Phy
                         USB_USB_MUXING_SOFTCON_BITS              ; // TODO: What is this?
-    usb_hw->pwr       = USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS | // Enable VBUS detection
-                        USB_USB_PWR_VBUS_DETECT_BITS             ; // Enable VBUS detection
+    usb_hw->pwr       = USB_USB_PWR_VBUS_DETECT_BITS             | // Enable VBUS detection
+                        USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS ; // Enable VBUS detection
     usb_hw->main_ctrl = USB_MAIN_CTRL_CONTROLLER_EN_BITS         | // Enable controller
                         USB_MAIN_CTRL_HOST_NDEVICE_BITS          ; // Enable USB Host mode
     usb_hw->sie_ctrl  = USB_SIE_CTRL_BASE                        ; // Default SIE_CTRL bits
@@ -183,8 +183,8 @@ void usb_host_reset() {
                         USB_INTE_ERROR_DATA_SEQ_BITS             | // Data error
                         USB_INTE_ERROR_RX_TIMEOUT_BITS           ; // Receive timeout
 
-    // usb_setup_endpoints();
     usb_setup_endpoint(&epx);
+    // usb_setup_endpoints();
     irq_set_enabled(USBCTRL_IRQ, true); // irq_set_exclusive_handler(USBCTRL_IRQ, isr_usbctrl);
     printf("\nUSB host reset\n\n");
 }
