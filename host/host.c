68a183bfb1f81d54442bef5580398b8b29454df1
Steve Shreeve <steve.shreeve@gmail.com>
2024-02-04 02:08:33 -0700

    Some temporary debugging

--- host/host.c
+++ host/host.c
@@ -697,12 +697,13 @@ void isr_usbctrl() {
         // Panic if we missed any buffers
         if (bits) panic("Unhandled buffer(s) %d\n", bits);
 
-        // if (len) {
-        //     printf("│> Data");
-        //     hexdump(usbh_dpram->epx_data, len, 1);
-        // } else {
-        //     printf("│<ZLP\n"); // which direction?!
-        // }
+        uint8_t len = 8;
+        if (len) {
+            printf("│> Data");
+            hexdump(usbh_dpram->epx_data, len, 1);
+        } else {
+            printf("│<ZLP\n"); // which direction?!
+        }
 
         // set_device_address();
         // get_device_descriptor();
