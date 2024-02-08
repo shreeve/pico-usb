f90ce194ec048f52fbd52381db4f1e903bcc0263
Steve Shreeve <steve.shreeve@gmail.com>
2024-02-07 23:47:30 -0700

    Alter expected data_pid for in/out transactions

--- host/host.c
+++ host/host.c
@@ -553,9 +553,10 @@ void start_control_transfer(endpoint_t *ep, usb_setup_packet_t *packet) {
     dar = dev_addr << USB_ADDR_ENDP_ENDPOINT_LSB
         | ep->ep_num;
     ecr = usbh_dpram->epx_ctrl;
-    bcr = (in  ? 0 : USB_BUF_CTRL_FULL)     // 0=Empty/Recv, 1=Full/Send
-        |            USB_BUF_CTRL_LAST      // Will fire TRANS_COMPLETE
-        |            USB_BUF_CTRL_DATA1_PID // Set next IN/OUT to DATA1
+    bcr = (in  ? 0 : USB_BUF_CTRL_FULL)      // 0=Empty/Recv, 1=Full/Send
+        |            USB_BUF_CTRL_LAST       // Will fire TRANS_COMPLETE
+        | (in  ?     USB_BUF_CTRL_DATA1_PID  // Next IN should be DATA1
+               :     USB_BUF_CTRL_DATA0_PID) // Next OUT should be DATA0
         | size;
     scr =            USB_SIE_CTRL_BASE               // SIE_CTRL defaults
      // | (fs  ? 0 : USB_SIE_CTRL_PREAMBLE_EN_BITS); // Preamble (LS on FS hub)
