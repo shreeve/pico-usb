8cb7c237afca5b1405506a6305f05075151aa91e
Steve Shreeve <steve.shreeve@gmail.com>
2024-02-12 11:15:27 -0700

    Slight tweaks to endpoint structure

--- host/host.c
+++ host/host.c
@@ -158,8 +158,8 @@ void reset_endpoint(endpoint_t *ep, usb_endpoint_descriptor_t *usb) {
         .ep_addr    = usb->bEndpointAddress, // Endpoint address
         .maxsize    = 0,                     // Maximum packet size
         .type       = usb->bmAttributes,     // Control, bulk, int, iso
-        .interval   = usb->bInterval,        // Polling interval in ms
         .data_pid   = 1,                     // Toggle DATA0/DATA1
+        .interval   = usb->bInterval,        // Polling interval in ms
         .active     = false,                 // Transfer is active
         .data_buf   = usbh_dpram->epx_data,  // Data buffer
         .user_buf   = NULL,                  // User buffer
