868d783adc82c14e7cfee4f742d0a58a0f331c1d
Steve Shreeve <steve.shreeve@gmail.com>
2024-02-13 15:00:09 -0700

    Update introductory comments

--- host/host.c
+++ host/host.c
@@ -6,8 +6,8 @@
 //   Note: This is a work in progress. It is not yet functional.
 //  Legal: Same license as the Raspberry Pi Pico SDK
 //
-// Thanks to the TinyUSB project for inspiration and code snippets! Also, thank
-// you Miroslav Nemecek for the https://github.com/Panda381/PicoLibSDK project.
+// Thanks to the TinyUSB project for inspiration. Also, Miroslav Nemecek's work
+// on https://github.com/Panda381/PicoLibSDK was very helpful.
 // ============================================================================
 
 #include <stdio.h>                // For printf
