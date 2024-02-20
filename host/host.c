b2df16482429b66d541018fc5df3e8973714437d
Steve Shreeve <steve.shreeve@gmail.com>
2024-02-19 21:59:43 -0700

    Remove old comment

--- host/host.c
+++ host/host.c
@@ -154,7 +154,6 @@ void reset_endpoint(endpoint_t *ep, usb_endpoint_descriptor_t *usb) {
     usbh_dpram->epx_ctrl = ecr;
 }
 
-// Reset the EPX endpoint // TODO: Make this generic and accept ep_addr, mps, interval, etc. or maybe another EP to copy from?
 SDK_INLINE void reset_epx() {
     reset_endpoint(epx, &((usb_endpoint_descriptor_t) {
         .bLength          = sizeof(usb_endpoint_descriptor_t),
