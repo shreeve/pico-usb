c9201576704c2d347b49aeaff392a8fd9d3f2ab0
Steve Shreeve <steve.shreeve@gmail.com>
2024-02-28 22:18:43 -0700

    Update comment

--- host/host.c
+++ host/host.c
@@ -421,7 +421,7 @@ void transfer(endpoint_t *ep) {
     // Perform the transfer (also gives SCR some time to settle)
     usb_hw->dev_addr_ctrl = dar;
     usb_hw->sie_ctrl      = scr & ~USB_SIE_CTRL_START_TRANS_BITS;
-    send_buffers(ep); // To send new transfers, they need this and help from SIE
+    send_buffers(ep); // New transfers need this and help from SIE's START_TRANS
     usb_hw->sie_ctrl      = scr;
 }
 
